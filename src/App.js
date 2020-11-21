f900db9cef903e82efdf6b34e325746dd0acbe11
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2020-11-21 12:47:54 +0100

    tailwind fix

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
-import "tailwindcss/tailwind.css";
+import "tailwindcss/dist/tailwind.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h2 className="text-red-600 bg-blue-800">Hello World</h2>
     </div>
   );
 }
